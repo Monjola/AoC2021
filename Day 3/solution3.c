01f09a51d8be8171e350b5ab85ef69260e387343
Monjola <mans.n@hotmail.com>
2021-12-03 08:31:50 +0100

    refactor day 3

--- Day 3/solution3.c
+++ Day 3/solution3.c
@@ -3,28 +3,21 @@
 #include <string.h>
 #include <math.h>
 char line[16];
+int nr_1s_per_col[16];
 int gamma_rate[16];
-int bin_gamma_rate = 0;
 int epsilon_rate[16];
-int bin_epsilon_rate = 0;
-int main() {
 
-FILE * input = fopen("input3.txt","r");
-
-    if (input == NULL) {
-        printf("Error, could not open file!");
-        exit(-1);
-    }
-
+void nr_of_1s_per_col(FILE * input) {
     for (int i = 0; i < 12; i++){
         while (fgets(line,15,input) !=NULL) {
-            gamma_rate[i] += line[i] - '0';
+            nr_1s_per_col[i] += line[i] - '0';
     }
-    printf("%d\n",gamma_rate[i]);
     rewind(input);
     }
+}
+void make_bin_arrays(void) {
     for (int i = 0; i < 12; i++) {
-        if (gamma_rate[i] < 500) {
+        if (nr_1s_per_col[i] < 500) {
             gamma_rate[i]=0;
             epsilon_rate[i]=1;
         }
@@ -32,19 +25,30 @@ FILE * input = fopen("input3.txt","r");
             gamma_rate[i]=1;
             epsilon_rate[i]=0;
         } 
-        printf("%d\n",epsilon_rate[i]);
     }
-    
-    
+}
+
+int bin_arr_to_bin_int(int *bin_arr,int length) {
+    int int_bin=0;
     int exp = 0;
-    for (int i = 11; i >= 0; i--) {
-        bin_gamma_rate += gamma_rate[i]* pow(2, exp);
-        bin_epsilon_rate += epsilon_rate[i]* pow(2,exp);
+    for (int i = length; i >= 0; i--) {
+        int_bin += bin_arr[i]* pow(2, exp);
         exp +=1;
     }
+}
+int main() {
     
-    printf("%d\n",bin_gamma_rate);
-    printf("%d\n",bin_epsilon_rate);
+    FILE * input = fopen("input3.txt","r");
+
+    if (input == NULL) {
+        printf("Error, could not open file!");
+        exit(-1);
+    }
+    nr_of_1s_per_col(input);
+    make_bin_arrays();
+    
+    int bin_gamma_rate = bin_arr_to_bin_int(gamma_rate,11);
+    int bin_epsilon_rate = bin_arr_to_bin_int(epsilon_rate,11);
 
     printf("%d\n", bin_gamma_rate*bin_epsilon_rate);
 }
